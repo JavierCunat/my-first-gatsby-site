7b688c1617211fb1c0e2a94b02c538924767c713
Javier Cunat <javiercunnat@gmail.com>
2022-12-01 14:44:27 -0500

    Initial Commit

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react' 
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Me</h1>
+            <p>Hi there, my name is Javier Cunat and I'm a firsy year Honors College Computer Science Major at Miami Dade College</p>
+        </main>
+    )
+}
+
+export const Head = () => <title> About Me </title>
+
+export default AboutPage
